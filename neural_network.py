a443a7de7275eb691625595b36dfce4df481a586
Jonas Kramer <jokramer2000@gmail.com>
2025-12-02 13:33:57 +0100

    feat(optimizer): Implement Adam-style parameter optimization in zMLPsuperclass

--- neural_network.py
+++ neural_network.py
@@ -3,36 +3,42 @@ from abc import ABC, abstractmethod
 
 from numpy.testing import assert_array_almost_equal
 import numpy as np
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris, make_classification
 from preprocessing import zStandardScaler
-from sklearn.neural_network import MLPRegressor
+from sklearn.neural_network import MLPRegressor, MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from preprocessing import zLabelBinarizer, ztrain_test_split
 
 class zMLPsuperclass:
-    def __init__(self, hidden_layer_sizes: tuple, random_state: int = 0,
+    def __init__(self, hidden_layer_sizes: tuple = (1,), random_state: int = 0,
                  activation: {"identity", "logistic", "tanh", "relu"} = "relu", 
-                 alpha: float = 0.0001, batch_size: float = 1.0, shuffle: bool = True,
-                 learning_rate_init: float = 0.001, max_iter: int = 200):
+                 alpha: float = 0.0001, batch_size: int = 1, shuffle: bool = True,
+                 learning_rate_init: float = 0.001, max_iter: int = 200, adam_beta_vel: float = 0.9, adam_beta_smoothness: float = 0.99):
         """
-        solver -> stochastic batch gradient descent
+        solver -> adam (only momentum, no division by sqrt(sum(squared gradients exponentially weighted)))
         learning_rate -> constant
         """
         self.hidden_layer_sizes = hidden_layer_sizes
         self.random_state = random_state
         self.activation = activation
-        self.alpha_lambda = alpha 
+        self.alpha = alpha 
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.learning_rate_init = learning_rate_init
         self.max_iter = max_iter
         self.rgen_ = np.random.default_rng(seed=self.random_state)
+        self.adam_beta_vel = adam_beta_vel
+        self.adam_beta_smoothness = adam_beta_smoothness
 
     def initialize_weights(self, input_dim, output_dim):
         Ws, bs = [], []
         dim_in = input_dim
         for i, dim_out in enumerate(self.hidden_layer_sizes + (output_dim, )):
             scaling = np.sqrt(1.0 / dim_in)
-            if(self.activation == "relu" and i != len(self.hidden_layer_sizes)):
+            if(self.activation == "relu"):
                 scaling = np.sqrt(2.0 / dim_in)
+            if(self.activation == "logistic"):
+                scaling = np.sqrt(1.0 / (dim_in + dim_out))
             W = self.rgen_.standard_normal(size=(dim_out, dim_in)) * scaling
             b = np.zeros(shape=(dim_out,))
             Ws.append(W)
@@ -57,7 +63,8 @@ class zMLPsuperclass:
     def get_batches(self, X, y):
         assert(isinstance(X, np.ndarray) and isinstance(y, np.ndarray))
         N_total = X.shape[0]
-        size = int(self.batch_size * N_total)
+        # size = int(self.batch_size * N_total)
+        size = self.batch_size
         if(self.shuffle):
             permutation = self.rgen_.permutation(N_total)
             X = X[permutation]
@@ -73,14 +80,23 @@ class zMLPsuperclass:
             y_batches.append(y[s:e])
             s = e
         return X_batches, y_batches
+    
+    def initialize_adam_parameters(self):
+        self.Ws_velocity_ = [np.zeros_like(W) for W in self.Ws_]
+        self.Ws_smoothness_ = [np.zeros_like(W) for W in self.Ws_]
+        self.bs_velocity_ = [np.zeros_like(b) for b in self.bs_]
+        self.bs_smoothness_ = [np.zeros_like(b) for b in self.bs_]
+
 
     def fit(self, X, y):
+        print("fit with shape X: ", X.shape, " shape y: ", y.shape)
         learning_rate = self.learning_rate_init
         assert(isinstance(X, np.ndarray) and isinstance(y, np.ndarray))
         if(len(y.shape) == 1): 
             y = y[:, np.newaxis]
         input_dim, output_dim = X.shape[1], y.shape[1]
         self.initialize_weights(input_dim, output_dim)
+        self.initialize_adam_parameters()
         self.loss_curve_ = []
         
         layer_sizes = self.hidden_layer_sizes + (output_dim,)
@@ -102,24 +118,45 @@ class zMLPsuperclass:
                     a_in = a_out
                 
                 # loss computation 
-                loss = self.compute_loss(a_out=a_in, y=y_batch)
-                self.loss_curve_.append(loss)
+                # loss = self.compute_loss(a_out=a_in, y=y_batch)
+                # self.loss_curve_.append(loss)
                 
                 # backpropagation
                 dLoss_dAout_output_layer = self.compute_dLoss_dAout_output_layer(a_out=a_in, y=y_batch)
                 dLoss_dAout = dLoss_dAout_output_layer
 
-                zipped_infos = tuple(zip(self.Ws_, self.bs_, layer_activations, a_ins, z_outs, a_outs))
+                zipped_infos = tuple(zip(self.Ws_, self.bs_, self.Ws_velocity_, self.bs_velocity_, self.Ws_smoothness_, self.bs_smoothness_, layer_activations, a_ins, z_outs, a_outs))
                 N = y_batch.shape[0]
-                for W, b, activation, a_in, z_out, a_out in zipped_infos[::-1]:
-                    dLoss_dAin, dLoss_dW, dLoss_db = self.compute_derivations_batch(a_in, z_out, a_out, W, dLoss_dAout, activation)
+                for W, b, W_vel, b_vel, W_smth, b_smth, activation, a_in, z_out, a_out in zipped_infos[::-1]:
+                    dLoss_dAin, dLoss_dW, dLoss_db = self.compute_derivations_batch(a_in, z_out, a_out, W, dLoss_dAout, activation, y_batch)
+                    # compute mean losses
+                    dLoss_dW = np.sum(dLoss_dW, axis=0)
+                    dLoss_db = np.sum(dLoss_db, axis=0)
 
-                    dW = learning_rate * (- dLoss_dW - self.alpha_lambda * (1.0 / N) * W * 2.0)
-                    db = learning_rate * (- dLoss_db)
-                    W += dW 
-                    b += db
+                    dW = (dLoss_dW + self.alpha * W) * (1.0 / N)
+                    # dW = learning_rate * (- dLoss_dW - self.alpha * W)
+                    db = (dLoss_db)
+
+                    W_vel = W_vel * self.adam_beta_vel + (1 - self.adam_beta_vel) * dW
+                    b_vel = b_vel * self.adam_beta_vel + (1 - self.adam_beta_vel) * db
+
+                    # removing the (1 - self.adam_beta_vel) bias
+                    W_vel_unbiased = W_vel / (1 - self.adam_beta_vel)
+                    b_vel_unbiased = b_vel / (1 - self.adam_beta_vel)
+
+                    # compute smoothness
+                    W_smth = W_smth * self.adam_beta_smoothness + (1 - self.adam_beta_smoothness) * dW**2
+                    b_smth = b_smth * self.adam_beta_smoothness + (1 - self.adam_beta_smoothness) * db**2
+
+
+                    W += - learning_rate * W_vel_unbiased / (np.sqrt(W_smth) + 1e-12)
+                    b += - learning_rate * b_vel_unbiased / (np.sqrt(b_smth) + 1e-12)
 
                     dLoss_dAout = dLoss_dAin # the ouput of the previous layer is the current input
+            
+            # a_out = self.predict(X) # the super is only used in derived classes, so it always uses the super of the superclas -> this is NOT a good solution, but a quick one # TODO
+            loss = self.compute_loss(X, y)
+            self.loss_curve_.append(loss)
         return self
 
     def predict(self, X):
@@ -147,21 +184,21 @@ class zMLPsuperclass:
         if(function == "identity"):
             return np.ones(shape=z_out.shape, dtype=z_out.dtype)
         
-    def compute_pre_activation(self, z_in, function: {"identity", "logistic", "tanh", "relu", "softmax"}):
-        assert(isinstance(z_in, np.ndarray))
+    def compute_pre_activation(self, z_out, function: {"identity", "logistic", "tanh", "relu", "softmax"}):
+        assert(isinstance(z_out, np.ndarray))
         assert(function in {"identity", "logistic", "tanh", "relu", "softmax"})
         if(function == "logistic"):
-            return 1.0 / (1 + np.exp(-1 * z_in))
+            return 1.0 / (1 + np.exp(-1 * z_out))
         if(function == 'tanh'):
-            return np.tanh(z_in)
+            return np.tanh(z_out)
         if(function == "relu"):
-            return np.where(z_in >= 0, z_in, 0.0)
+            return np.where(z_out >= 0, z_out, 0.0)
         if(function == "identity"):
-            return np.copy(z_in)
+            return np.copy(z_out)
         if(function == "softmax"):
-            e_raised_to_z = np.exp(z_in)
+            e_raised_to_z = np.exp(z_out)
             sum_e_raised_to_z = np.sum(e_raised_to_z, axis=1)
-            a_out = e_raised_to_z / sum_e_raised_to_z
+            a_out = e_raised_to_z / sum_e_raised_to_z[:,np.newaxis]
             return a_out
         
     def compute_activation(self, a_in, W, b, function: {"identity", "logistic", "tanh", "relu", "softmax"}):
@@ -189,7 +226,7 @@ class zMLPsuperclass:
         return dLoss_dAin, dLoss_dW, dLoss_db
     
     def compute_derivations_batch(self, a_in, z_out, a_out, W, dLoss_dAout, 
-                                          function: {"identity", "logistic", "tanh", "relu", "softmax"}):
+                                          function: {"identity", "logistic", "tanh", "relu", "softmax"}, y=None):
         """
         The implementation is not perfectly optimized for performance, but for readability.
         """
@@ -209,6 +246,8 @@ class zMLPsuperclass:
             # then compute dLoss_dZout and split into easier components
             # [N, out] * [N, out] + [N, out] * ( [N, out] scalarproducts [N, out]) = [N, otu] + [N, out] * [N] = [N, out] * [N, np.newaxis] = [N, out] (broadcasting)
             dLoss_dZout = dLoss_dAout * a_out + a_out * (np.sum(dLoss_dAout * a_out, axis=1))[:, np.newaxis]
+            if(y is not None):    # TODO: use one solution or other not both
+                dLoss_dZout = a_out - y
 
 
         # N times: column-vetor * row-vector = matrix -> it is easy to verify this formula by hand
@@ -221,55 +260,118 @@ class zMLPsuperclass:
         # chain rule
         dZout_dAin = W
         dLoss_dAin = dLoss_dZout @ dZout_dAin # [N, out] @ [out, in] = [N, in]
-
-        # compute mean losses
-        dLoss_dW = np.mean(dLoss_dW, axis=0)
-        dLoss_db = np.mean(dLoss_db, axis=0)
         return dLoss_dAin, dLoss_dW, dLoss_db
     
 class zMLPRegressor(RegressorMixin, BaseEstimator, zMLPsuperclass):
-    def __init__(self, hidden_layer_sizes: tuple, random_state: int = 0,
-                 activation: {"identity", "logistic", "tanh", "relu"} = "relu", 
-                 alpha: float = 0.0001, batch_size: int = 100, shuffle: bool = True,
-                 learning_rate_init: float = 0.001, max_iter: int = 200):
-        super().__init__(hidden_layer_sizes, random_state, activation, alpha, batch_size, shuffle, learning_rate_init, max_iter)
-    
     def get_output_activation_function(self):
         return "identity"
 
     def compute_dLoss_dAout_output_layer(self, a_out, y):
         assert(isinstance(a_out, np.ndarray) and isinstance(y, np.ndarray))
-        N = a_out.shape[0]
-        dLoss_dAout = (1.0 / N) * (a_out - y) * 2.0
+        dLoss_dAout = (a_out - y) * 2.0
         return dLoss_dAout
     
-    def compute_loss(self, a_out, y):
+    def compute_loss(self, X, y):
+        a_out = self.predict(X)
         assert(isinstance(a_out, np.ndarray) and isinstance(y, np.ndarray))
         N = a_out.shape[0]
         Loss = np.mean((a_out - y)**2, axis=(0, 1))
         return Loss
     
-            
+class zMLPClassifier(ClassifierMixin, BaseEstimator, zMLPsuperclass):
+    def __init__(self, hidden_layer_sizes = (1, ), random_state = 0, activation = "relu", alpha = 0.0001, 
+                 batch_size = 1, shuffle = True, learning_rate_init = 0.001, max_iter = 200, adam_beta_vel = 0.9, adam_beta_smoothness = 0.99,
+                 output_activation = 'softmax'):
+        super().__init__(hidden_layer_sizes, random_state, activation, alpha, batch_size, shuffle, learning_rate_init, max_iter, adam_beta_vel, adam_beta_smoothness)
+        self.output_activation = output_activation
 
-if __name__ == "__main__":
-    X, y = load_iris(return_X_y=True)
-    use_custom_regressor = True
-    nn = zMLPRegressor(hidden_layer_sizes=(3, 2), activation="identity", batch_size=0.05, learning_rate_init=0.001, alpha=0.1, max_iter=200)
-    if(not use_custom_regressor):
-        nn = MLPRegressor(hidden_layer_sizes=(3, 2), activation="identity", batch_size=(int)(0.05 * 150), learning_rate_init=0.001, learning_rate="constant",
-                          alpha=0.1, max_iter=200)
+    def get_output_activation_function(self):
+        return self.output_activation
+    
+    def fit(self, X, y):
+        unique_elements = np.unique(y)
+        unique_elements.sort()
+        assert(unique_elements[0] == 0 and unique_elements[1] == 1 and len(unique_elements) == 2) # only except one hot encoded targets
+        super().fit(X, y)
+        return self
+
+    def predict_proba(self, X):
+        Y = super().predict(X)
+        return Y 
+    
+    def predict(self, X):
+        Y = self.predict_proba(X)
+        Y_max = np.max(Y, axis=1)
+        Y_onehot = np.where(Y == Y_max[:, np.newaxis], 1, 0)
+        return Y_onehot
+    
+    """
+    not used in current implementation
+    """
+    def compute_dLoss_dAout_output_layer(self, a_out, y):
+        assert(isinstance(a_out, np.ndarray) and isinstance(y, np.ndarray))
+        dLoss_dAout = (a_out - y) # / (a_out * (1 - a_out))
+        return dLoss_dAout
+    
+    def compute_loss(self, X, y):
+        a_out = self.predict_proba(X)
+        assert(isinstance(a_out, np.ndarray) and isinstance(y, np.ndarray))
+        N = a_out.shape[0]
+        log_losses = np.where(a_out > 0.0001, - y * np.log(a_out), 10000)
+        log_losses = - y * np.log(a_out)
+        Loss = np.mean(log_losses, axis=(0, 1))
+        return Loss
+
+    
+# the regularization error does not get propagated
+def run_classifier_test():
+    X, y = make_classification(n_samples=10000, n_features=10, n_classes=2)
     sc = zStandardScaler()
     X = sc.fit_transform(X)
-    nn.fit(X, y)
-    nn.predict(X)
-    print(nn.score(X, y))
-    
+    print("y.shape: ", y.shape)
+    X_train, X_test, y_train, y_test = ztrain_test_split(X, y, test_size=0.2, stratify=y)
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+    lb = zLabelBinarizer()
+    y_test = lb.fit_transform(y_test)
+    y_train = lb.transform(y_train)
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+    cl = zMLPClassifier(random_state=2, hidden_layer_sizes=(2,), activation="logistic", batch_size=10, output_activation="softmax", alpha=0.1, max_iter=100, learning_rate_init=0.00001)
+    cl.fit(X_train, y_train)
+    print(cl.score(X_train, y_train))
+    print(cl.score(X_test, y_test))
+
     import matplotlib.pyplot as plt 
-    plt.plot(nn.loss_curve_)
+    plt.plot(cl.loss_curve_)
     plt.show()
 
-# TODO: change dividing by N_batch_size to dividing by N_total_size --> otherwise the model overshoots
 
+def run_regressor_test():
+    X, y = load_iris(return_X_y=True)
+    batch_size = 10
+    activation = "identity" # works for logistic, tanh, identity
+    learning_rate_init = 0.0001
+    alpha = 0
+    max_iter = 200
+    nn = zMLPRegressor(hidden_layer_sizes=(3, 2), activation=activation, batch_size=batch_size, learning_rate_init=learning_rate_init, alpha=alpha, max_iter=max_iter)
+    nn2 = MLPRegressor(hidden_layer_sizes=(3, 2), activation=activation, batch_size="auto", learning_rate_init=10*learning_rate_init, learning_rate="constant",
+                          alpha=alpha, max_iter=max_iter, solver="sgd")
+    sc = StandardScaler()
+    X = sc.fit_transform(X)
+    nn.fit(X, y) ; nn2.fit(X, y)
+    nn.predict(X) ; nn2.fit(X, y)
+    print(nn.score(X, y), nn2.score(X, y))
+    
+    import matplotlib.pyplot as plt 
+    plt.plot(np.arange(len(nn.loss_curve_)), nn.loss_curve_, color="green", label="custom")
+    plt.plot(np.arange(len(nn.loss_curve_), step=int(len(nn.loss_curve_)/len(nn2.loss_curve_)))[:len(nn2.loss_curve_)], nn2.loss_curve_, color="red", label="original")
+    plt.legend(loc="best")
+    plt.show()
+
+    
+if __name__ == "__main__":
+    run_classifier_test()
+
+# TODO: add get_params and set_params
 
 def setup_batch_data(function, N=4, dimIn=2, dimOut=3):
     """Sets up batch data (N=4, dimIn=2, dimOut=3) and common parameters."""
